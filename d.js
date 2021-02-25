fa1808374be41144e8a352ef52aec4f367f677b6
naquangaston <35858201+naquangaston@users.noreply.github.com>
2021-02-24 21:53:21 -0500

    Add files via upload

---
+++ d.js
@@ -0,0 +1,128 @@
+var http = require('http');
+let fs = require('fs');
+var files = [];
+var tfiles = 0;
+function Fs(filename, data) {
+    fs.writeFileSync(filename, data);
+    return;
+}
+function Shell(command) {
+    const { exec } = require("child_process");
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
+function ask(txt) {
+    let r
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    readline.question(txt ? txt : '', name => {
+        r = name;
+    });
+    return r;
+}
+function render(_view, data) {
+    var view = render.views[view];
+    for (var key in data) {
+        var value = data[key];
+        view.replace("{{" + key + "}}", value);
+    }
+    return view;
+}
+
+render.views = {
+    "someView": "<p>{{foo}}</p>"
+};
+const https = require('https');
+
+/**
+ * getJSON:  RESTful GET request returning JSON object(s)
+ * @param options: http options object
+ * @param callback: callback to pass the results JSON object(s) back
+ */
+
+module.exports.getJSON = (options, onResult, a = 0) => {
+    console.log('rest::getJSON');
+    const port = options.port == 443 ? https : http;
+
+    let output = '';
+
+    {
+        const req = port.request(options, (res) => {
+            console.log(`${options.host} : ${res.statusCode}`);
+            res.setEncoding('utf8');
+
+            res.on('data', (chunk) => {
+                output += chunk;
+            });
+
+            res.on('end', () => {
+                let obj
+                try { obj = JSON.parse(output); } catch (err) { obj = output }
+
+                onResult(res.statusCode, obj);
+            });
+        });
+
+        req.on('error', (err) => {
+            // res.send('error: ' + err.message);
+        });
+
+        req.end();
+    };
+}
+async function fA(name, host, path, port = 443) {
+    await new Promise((a2, b2) => {
+        module.exports.getJSON(
+            {
+                port: port,
+                host: host,
+                path: path
+            },
+            (a, b) => {
+                tfiles++;
+                console.log('status:', a)
+                console.log('lines:', b.split('\n').length);
+                console.log('chars:', b.split('').length);
+                console.log('Adding file');
+                files.push([name, b]);
+                console.log('added', name)
+                console.log('Total files', files.length)
+                a2();
+
+            })
+    })
+    return;
+}
+function De(url) {
+    var pathArray = url.split('/')
+    var pro = pathArray[0];
+    var dom = pathArray[2];
+    var newPathname = "";
+    for (i = 2; i < pathArray.length; i++) {
+        newPathname += "/";
+        newPathname += pathArray[i];
+    }
+    return {proc:pro, host:dom, path:url.split(dom)[1] };
+}
+(async () => {
+    await fA('Mods.html', 'raw.githubusercontent.com', '/naquangaston/idk/main/Mods.html');
+    console.log('Version', eval(files[0][1].split('</h1></center>')[0].split('<center><h1>V-')[1]))
+    var urls = [];
+    urls.push (De('https://raw.githubusercontent.com/naquangaston/idk/main/README.md'));
+    urls.push(De('https://raw.githubusercontent.com/naquangaston/idk/main/vrcUpdater.cmd'))
+    await fA('Read_me.txt', urls[0].host, urls[0].path);
+    await fA('Installer.bat', urls[1].host, urls[1].path);
+    files.forEach(e => { Fs(e[0], e[1]) })
+})()
